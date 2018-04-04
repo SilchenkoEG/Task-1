b1521d486a8064479c6ca0f4d8d051c9e1de90ca
UkrMedved <silchenkoEG@gmail.com>
2018-04-04 13:04:58 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,76 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
-import data from './data.json';
-
-
-
+import Category from './Category'
 
 class App extends Component {
-  constructor (props){
-    super(props);
-    this.state = {
-      correctCategory : [],
-      filterCategory:[],
-      checked: true
-      // searchString : ''
-    }
-  }
-  showCategory = (name) => {
-    data.map(element => {
-      if(element.name === name){
-        this.setState({ correctCategory :
-        element.children,filterCategory: element.children })
-      }})
-      console.log(this.state.filterCategory)
-  }
-  changeSearch = (event) => {
-    console.log(isNaN(event.target.value))
-    this.setState({filterCategory: 
-      this.state.correctCategory.filter(item => item.title.indexOf(event.target.value) !== -1)
-     })
-  }
-  changeCheck = (event) => {
-    // console.log(event.target.checked)
-    // console.log(event.target.value);
-    if(event.target.checked === true){
-      this.setState({filterCategory:
-        this.state.correctCategory.filter(item => item.color === event.target.value)
-      })
-    }
-    else {
-      this.setState({filterCategory: this.state.correctCategory});
-    }
-  }
   render() {
-    let dropDownText;
-    if(this.state.filterCategory != []){
-      dropDownText = <div>
-      {this.state.filterCategory.map(item => { return <p key={item.title} >{item.title} {item.price}</p>} )}
-    </div> 
-    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> React</h1>
-        </header>
-      <div className='filterColor'>
-      <p> <input type='checkbox'  value='black' onChange={this.changeCheck}/>Black</p>
-      <p><input type='checkbox'  onChange={this.changeCheck} value='white' />White </p>   
-      </div>  
-        <input 
-          type ='text'
-          // value = {this.state.searchString} 
-          onChange = {this.changeSearch}
-        />
-        <ul>
-          {data.map((category, index)=>{
-            return <li key={index} onClick={ () => this.showCategory(category.name)}>{category.name}</li>
-          })}
-        </ul>
-        {dropDownText} 
-      </div>
+      <Category/>
     );
   }
 }
---
+++ src/Category.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import data from './data.json'
+
+class Category extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      filterNameCategory:'',
+      filterColor:['black', 'white'],
+      filterName:'',
+    }    
+  }
+  // showCategory = (name) => {
+  //   data.map(element => {
+  //     if(element.name === name){
+  //       this.setState({ correctCategory :
+  //       element.children,filterCategory: element.children })
+  //     }})
+  // }
+  showCategory = (name) => {
+    this.setState({ filterNameCategory : name});
+  }
+  changeSearch = (event) => {
+    this.setState({ filterName: event.target.value })
+  }
+  changeCheck = (event) => {
+    if(event.target.checked){
+      const joined = this.state.filterColor.slice()
+      joined.push(event.target.value);
+      this.setState({filterColor : joined })
+    }
+    else{
+      const joined = this.state.filterColor;
+      joined.splice(joined.indexOf(event.target.value),1)
+      this.setState({filterColor : joined})
+    }
+  }
+    render() {
+      return (
+        <div className="App">
+          <div className='filterColor'>
+            <p><input type='checkbox' value='black' checked={this.state.filterColor.includes('black')} onChange={this.changeCheck} />Black</p>
+            <p><input type='checkbox' value='white' checked={this.state.filterColor.includes('white')} onChange={this.changeCheck} />White </p>   
+          </div>  
+          <input 
+            type ='text' value = {this.state.filterName} onChange = {this.changeSearch}/>
+          <ul>
+          {data.map((category)=>{
+            return <li key={category.name} onClick={ () => this.showCategory(category.name)}>{category.name}</li>
+          })}
+        </ul>
+        { data.map(item => item.children).reduce((a,b) => a.concat(b))
+          .filter((product)=> this.state.filterColor.includes(product.color))
+          .filter( item => item.title.indexOf(this.state.filterName) !== -1)
+          .map((item,index) =>  <p key={index} >{item.title} {item.price} {item.color}</p> )}
+         </div> 
+      );
+    }
+  }
+  
+  export default Category;
